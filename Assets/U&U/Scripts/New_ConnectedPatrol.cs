4c46face0ac4227e6f7fee5dbf78dc0ed448bc00
CrimsonHao <arq.losorno@gmail.com>
2020-06-22 19:52:48 -0500

    Script completado de persecusion 3/4 's

--- Assets/U&U/Scripts/New_ConnectedPatrol.cs
+++ Assets/U&U/Scripts/New_ConnectedPatrol.cs
@@ -3,36 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+//Komoditos B4 Kulitos :3
 namespace Assets.Code
 {
     public class New_ConnectedPatrol : MonoBehaviour
     {
-        enum EnemyState { Patrol, Chase };
+        enum EnemyState { Patrol, Initializing, Chase };
 
         [SerializeField] bool patrolWaiting; //Decide si el agente espera en cada nodo o no
-
         [SerializeField] float totalWaitTime = 3f; //Tiempo total de espera en el nodo
-
         [SerializeField] float switchProb = 0.2f; //Probabilidad de cambio de direccion
+        [SerializeField] EnemyState currentState; //Estado actual del enemigo
+        [SerializeField] EnemyState previousState; //Estado previo del enemigo
+        [SerializeField] Transform player; //Jugador
+        [SerializeField] float rayDistance; //Distancia del raycast
+        [SerializeField] bool isLookingAtPlayer; //Si el enemigo esta viendo al jugador
+        [SerializeField] LayerMask playerMask; //Layer que interactuara con el ray
 
-        [SerializeField] EnemyState currentState; //Estado del enemigo
+        NavMeshAgent navMeshAgent;
+        New_Waypoint currentWaypoint;
+        New_Waypoint previousWaypoint;
 
-        NavMeshAgent navMeshAgent;                                                                                  //Variables de comportamiento
-        New_Waypoint currentWaypoint;                                                                               //
-        New_Waypoint previousWaypoint;                                                                              //
-                                                                                                                    //
-        bool isTraveling;                                                                                           //
-        bool isWaiting;                                                                                             //
-        float waitTimer;                                                                                            //
-        int waypointsVisited;                                                                                       //
+        Ray detectionRay;
+        RaycastHit hit;
+
+        private bool isTraveling;  //Si esta viajando
+        private bool isWaiting; //Si esta esperando
+        private float waitTimer; //Se almacena el tiempo a comparar para la espera
+        private int waypointsVisited; // Cuantos waypoints hemos visitado
+        private float playerDistance; //Distanca al jugador
+        private bool isPreparing; //Si esta en espera de cambiar de estado
 
         public void Start()
         {
+            currentState = EnemyState.Patrol; //En un inicio el enemigo esta en el estado de patrulla
             navMeshAgent = this.GetComponent<NavMeshAgent>();  //Se asigna el componente "NavMeshAgent" a la variable
 
             if (navMeshAgent == null) //Revisa en caso que no tenga el componente
             {
-                Debug.LogError("The nav mesh agent component is not ettached to " + gameObject.name);
+                Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
             }
             else
             {
@@ -67,31 +76,86 @@ namespace Assets.Code
 
         public void Update()
         {
-            //Check if it is close to the destination
-            if(isTraveling && navMeshAgent.remainingDistance <= 1.0f)
-            {
-                isTraveling = false;
-                waypointsVisited++;
+            playerDistance = Vector3.Distance(this.transform.position, player.transform.position); //Establece la distancia al jugador EN.TODO.MOMENTO.
+            detectionRay = new Ray(this.transform.position, this.transform.forward); //Se crea el Ray
+            Debug.DrawRay(detectionRay.origin, detectionRay.direction * rayDistance, Color.red); //Se dibuja el ray
 
-                //Check if we are waiting, then wait
-                if(patrolWaiting)
+            if(Physics.Raycast(detectionRay, out hit, rayDistance, playerMask) && currentState == EnemyState.Patrol) //Si detecta al jugador y no esta en estado de chase
+            {
+                navMeshAgent.isStopped = true;
+
+                transform.LookAt(player);
+                isLookingAtPlayer = true;
+                Debug.LogWarning("Player is in VISION RANGE");
+
+                currentState = EnemyState.Initializing;
+                previousState = EnemyState.Patrol;
+                isPreparing = true;
+            }
+            else if(Physics.Raycast(detectionRay, out hit, rayDistance, playerMask) && currentState == EnemyState.Chase)
+            {
+                transform.LookAt(player);
+                isLookingAtPlayer = true;
+                Debug.LogWarning("Player is being chased");
+            }
+            else
+            {
+                currentState = EnemyState.Patrol;
+                previousState = EnemyState.Patrol;
+            }
+
+            if (currentState == EnemyState.Initializing && isPreparing == true)
+            {
+                StartCoroutine(ChaseTransition());
+                isPreparing = false;
+            }
+            else if(currentState == EnemyState.Patrol) //Revisa si el estado del enemigo es Patrullaje
+            {
+                if (isTraveling && navMeshAgent.remainingDistance <= 1.0f) //Revisa si esta cerca del destino
                 {
-                    isWaiting = true;
-                    waitTimer = 0f;
+                    isTraveling = false;
+                    waypointsVisited++;
+
+                    if (patrolWaiting) //Si se tiene que esperar, espera
+                    {
+                        isWaiting = true;
+                        waitTimer = 0f;
+                    }
+                    else
+                    {
+                        SetDestination(); //Obvio
+                    }
+                }
+
+                if (isWaiting) //Si se esta esperando
+                {
+                    waitTimer += Time.deltaTime;
+                    if (waitTimer >= totalWaitTime)
+                    {
+                        isWaiting = false;
+                        patrolWaiting = false;
+                        SetDestination(); //Obvio
+                    }
+                }
+            }
+            else if(currentState == EnemyState.Chase)
+            {
+                if(isLookingAtPlayer)
+                {
+                    previousState = EnemyState.Chase;
+
+                    if(navMeshAgent.remainingDistance <= 0.8f)
+                    {
+                        currentState = EnemyState.Initializing;
+                        isPreparing = true;
+                        Debug.LogWarning("Ataque");
+                    }
                 }
                 else
                 {
-                    SetDestination();
-                }
-            }
-
-            // If you are waiting
-            if(isWaiting)
-            {
-                waitTimer += Time.deltaTime;
-                if(waitTimer >= totalWaitTime)
-                {
-                    isWaiting = false;
+                    currentState = EnemyState.Initializing;
+                    isPreparing = true;
+                    Debug.LogWarning("El jugador escapo");
                 }
             }
         }
@@ -108,6 +172,38 @@ namespace Assets.Code
             Vector3 targetVector = currentWaypoint.transform.position;
             navMeshAgent.SetDestination(targetVector);
             isTraveling = true;
+
+            float rand = UnityEngine.Random.Range(0.0f, 1.0f);
+            if (rand <= switchProb)
+            {
+                patrolWaiting = true;
+            }
+        }
+
+        private void SetChaseUp()
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+
+        IEnumerator ChaseTransition()
+        {
+            navMeshAgent.isStopped = true;
+            yield return new WaitForSeconds(2.5f);
+
+            if(previousState == EnemyState.Patrol && currentState == EnemyState.Initializing)
+            {
+                currentState = EnemyState.Chase;
+                previousState = EnemyState.Initializing;
+                navMeshAgent.isStopped = false;
+                SetChaseUp();
+            }
+            else if(previousState == EnemyState.Chase && currentState == EnemyState.Initializing)
+            {
+                currentState = EnemyState.Patrol;
+                previousState = EnemyState.Initializing;
+                navMeshAgent.isStopped = false;
+                SetDestination();
+            }
         }
     }
 }
